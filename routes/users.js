2960addcea173f18fd04f3c9cae5f6b69b8ef0cd
Darc Boykin <darcboykin@gmail.com>
2017-01-31 17:50:10 -0500

    add user profile page route, loginRequired helper method

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var authHelpers = require('../auth/auth-helper');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
